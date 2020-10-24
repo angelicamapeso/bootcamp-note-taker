d1f2dd5505ade5a1a57de00b8f8559da6fc59bea
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2020-10-23 23:47:57 -0400

    Add home path

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const PORT = 3000;
 //creates express application
 const app = express();
 
+app.get('/', function(req, res) {
+  res.send('Server has started!');
+});
+
 app.listen(PORT, () => {
   console.log(`App is listening on ${PORT}`);
 });
