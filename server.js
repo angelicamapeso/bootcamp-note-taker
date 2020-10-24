24f0c03a3d266f2c182a2b0da1a9a5a54c7aaf99
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2020-10-23 23:47:40 -0400

    Set app to listen on port 3000

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 const express = require('express');
 
+//define port
+const PORT = 3000;
+
 //creates express application
 const app = express();
+
+app.listen(PORT, () => {
+  console.log(`App is listening on ${PORT}`);
+});
