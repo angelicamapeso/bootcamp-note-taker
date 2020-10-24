ff452dff7da176e5074ebaff3e57164d9343b648
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2020-10-24 00:15:04 -0400

    Add middleware to serve static files
    Static files reference in home and notes, css and javascript

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const PORT = 3000;
 //creates express application
 const app = express();
 
+//to serve static files (css and javascript)
+app.use(express.static(path.join(__dirname, 'public')));
+
 //----- Paths -----//
 //Home
 app.get('/', function(req, res) {
