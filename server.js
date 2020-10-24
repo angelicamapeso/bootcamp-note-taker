7acb302c14c1728925348649ddaaabe779927ffa
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2020-10-24 01:55:03 -0400

    Add middleware to parse incoming data

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const app = express();
 
 //to serve static files (css and javascript)
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //----- Paths -----//
 //Home
