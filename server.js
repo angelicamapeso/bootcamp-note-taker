2c5c58a50cd381fa578a16985747d5b82ed23df7
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2020-10-24 00:37:17 -0400

    Add path for api/notes

--- server.js
+++ server.js
@@ -21,6 +21,15 @@ app.get('/notes', function(req, res) {
   res.sendFile(path.join(__dirname, 'public', 'notes.html'));
 });
 
+//API
+app.get('/api/notes', function(req, res) {
+  fs.readFile(path.join(__dirname, 'db', 'db.json'), (err, data) => {
+    if (err) throw err;
+    const notes = JSON.parse(data);
+    res.json(notes);
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`App is listening on ${PORT}`);
 });
