0e69f3a7c9c496bfa8b0697bad34ebe98cc806d9
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2020-10-25 14:28:25 -0400

    Add dynamic port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const fs = require('fs');
 
 //define port
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 //creates express application
 const app = express();
